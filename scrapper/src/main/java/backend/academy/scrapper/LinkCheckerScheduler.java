094a145376e1b892ba9ca75a27697bf0c8593424
Elina <elinanurgal@gmail.com>
2025-04-14 21:15:25 +0300

    master. separated bot client from scheduler

--- scrapper/src/main/java/backend/academy/scrapper/LinkCheckerScheduler.java
+++ scrapper/src/main/java/backend/academy/scrapper/LinkCheckerScheduler.java
@@ -1,27 +1,23 @@
 package backend.academy.scrapper;
 
-import backend.academy.bot.entity.LinkUpdate;
 import backend.academy.bot.entity.TrackedResource;
+import backend.academy.scrapper.client.BotClient;
 import backend.academy.scrapper.client.GithubClient;
 import backend.academy.scrapper.client.StackOverflowClient;
 import java.util.List;
-import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestClient;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @Component
 public class LinkCheckerScheduler {
     private static final Logger log = LoggerFactory.getLogger(LinkCheckerScheduler.class);
-    private final RestClient botClient;
+    private final BotClient botClient;
     private final GithubClient githubClient;
     private final StackOverflowClient stackoverflowClient;
 
-    public LinkCheckerScheduler(RestClient botClient, GithubClient githubClient, StackOverflowClient stackoverflowClient) {
+    public LinkCheckerScheduler(BotClient botClient, GithubClient githubClient, StackOverflowClient stackoverflowClient) {
         this.botClient = botClient;
         this.githubClient = githubClient;
         this.stackoverflowClient = stackoverflowClient;
@@ -30,28 +26,16 @@ public class LinkCheckerScheduler {
     @Scheduled(fixedRate = 5 * 60 * 1000)
     public void checkAllLinks() {
         try {
-            List<TrackedResource> resources = getTrackedResources();
+            List<TrackedResource> resources = botClient.getTrackedResources();
             for (TrackedResource resource : resources) {
                 boolean isUpdated = isUpdated(resource);
                 if (isUpdated) {
-                    sendUpdateNotification(resource);
+                    botClient.sendUpdateNotification(resource);
                 }
             }
         } catch (Exception e) {
-            log.error("Scheduler error", e);
+            log.error("Ошибка планировщика:", e);
         }
-    }
-
-    private void sendUpdateNotification(TrackedResource resource) {
-        botClient.post()
-            .uri("/api/updates")
-            .body(new LinkUpdate(
-                resource.getChatId(),
-                resource.getLink(),
-                "Обнаружены изменения"
-            ))
-            .retrieve()
-            .toBodilessEntity();
     }
 
     private boolean isUpdated(TrackedResource resource) {
@@ -59,16 +43,6 @@ public class LinkCheckerScheduler {
             case GITHUB_REPO, GITHUB_ISSUE, GITHUB_PR -> githubClient.hasUpdates(resource);
             case STACKOVERFLOW -> stackoverflowClient.hasUpdates(resource);
         };
-    }
-
-    private @Nullable List<TrackedResource> getTrackedResources() {
-        return botClient.get()
-            .uri(uriBuilder -> uriBuilder
-                .path("/api/links")
-                .queryParam("interval", "5m")
-                .build())
-            .retrieve()
-            .body(new ParameterizedTypeReference<>() {});
     }
 }
 
--- scrapper/src/main/java/backend/academy/scrapper/client/BotClient.java
+++ scrapper/src/main/java/backend/academy/scrapper/client/BotClient.java
@@ -1,11 +1,39 @@
 package backend.academy.scrapper.client;
 
+import backend.academy.bot.entity.LinkUpdate;
+import backend.academy.bot.entity.TrackedResource;
+import org.jetbrains.annotations.Nullable;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.web.client.RestClient;
+import java.util.List;
 
 public class BotClient {
     private final RestClient restClient;
 
     public BotClient(RestClient restClient) {
         this.restClient = restClient;
+    }
+
+    public void sendUpdateNotification(TrackedResource resource) {
+        restClient.post()
+            .uri("/api/updates")
+            .body(new LinkUpdate(
+                resource.getChatId(),
+                resource.getLink(),
+                "Обнаружены изменения"
+            ))
+            .retrieve()
+            .toBodilessEntity();
+    }
+
+    public  @Nullable List<TrackedResource> getTrackedResources() {
+        return restClient.get()
+            .uri(uriBuilder -> uriBuilder
+                .path("/api/links")
+                .queryParam("interval", "5m")
+                .build())
+            .retrieve()
+            .body(new ParameterizedTypeReference<>() {
+            });
     }
 }
